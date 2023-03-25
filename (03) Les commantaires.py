b9c3344c60cb13644c2b8b24a89e265cfef635c9
Gallium <96748006+TheGallium@users.noreply.github.com>
2023-03-25 16:16:26 +0100

    Ajout du cours 03: Les commentaire

---
+++ (03) Les commantaires.py
@@ -0,0 +1,67 @@
+
+print("   Comment donner des indications ou des   ")
+print("       explications dans ton code ???      ")
+
+
+#Utilise un "#" au début de la ligne pour créer un commentaire
+
+# Tu peut rajouter un espace apprès le "#" pour que le texte soit plus lisible !
+# Tu peut aussi créer un commentaire à la fin d'une ligne : (pas au début)
+
+un283 = list("dhzi82") # explication du code
+
+
+
+
+# Mais attention !!
+# Ne jamais décrire un code trop simple, sinon, c'est juste une perte de temps...
+# Exemple :
+
+print("coucou ca va ?") # demande si ca va
+
+
+
+
+# Si tu veut passer rapidement une ligne en commentaire:
+# Fait "ctrl + maj + égal".
+# La ligne séléctionnée va passer en commentaire
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # stylé ce séparateur fait de commentaire
+
+
+# Maintenant MÉTHODE 2:
+
+# Tu veut faire un commentaire de plusieurs lignes.
+# Tu pourrait faire plusieurs "#", plusieurs commentaires...
+# Ou faire ceci :
+
+# LES commentaire MULTILIGNES (multi lignes = plusieurs lignes)
+
+"""
+En appuiant trois fois sur la touche guillemets,
+puis en appuyant sur entrer, tu pourra écrire entre les trois guillemets.
+
+Ce code est exactement comme un fichier texte et ne sera pas executé.
+Je trouve que c'est plus beau est pratique pour de longues explications.
+
+Tu peut séléctionner un texte pas dans un commentaire et appuyer trois fois sur la touche guillemets
+
+Si tu veut je te laisse cet espace pour t'entrainer avec ces deux méthodes !
+"""
+
+
+
+
+
+
+
+
+
+
+"""
+Tu peut séléctionner un texte pas dans un commentaire et appuyer trois
+fois sur la touche guillemets pour le mettre en commentaire multiligne.
+"""
+
+# fin de l'espace d'entrainement (:
